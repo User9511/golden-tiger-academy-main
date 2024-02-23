b2e848a5e5c1a53f984a570c5825e50f2f0fbadc
Smgy94 <Shanemcgeown@gmail.com>
2024-02-23 11:35:19 +0000

    Slide  in animation for "Your First Class" tips
    Using an IntersectionObserver to create a sliding-in animation on scroll for each of the tips in the "Your First Class" section.

---
+++ animate.js
@@ -0,0 +1,14 @@
+// Slide in Animation for each "Tip" under Your First Class
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
